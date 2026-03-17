b0bc9fa7bfbbd2446c1ae816f910af08ce7140c9
Vuduc2907 <vduc9262@gmail.com>
2026-03-17 20:48:20 +0700

    nop bai tap

--- ss17/baitapvenha/bai3.js
+++ ss17/baitapvenha/bai3.js
@@ -1,22 +1,30 @@
-
-const body = document.body;
-const nut = document.getElementById("btn");
-
-let doiTheme = (themeMoi)  =>  {
-  if (themeMoi === "dark") {
-    body.className = "dark";
-  } else {
-    body.className = "light";
-  }
-  localStorage.setItem("theme", themeMoi);
+let h1 = document.getElementsByTagName("h1")[0];
+let btn = document.getElementById("btn");
+let body = document.getElementsByTagName("body")[0];
+let statusTheme = localStorage.getItem("Theme");
+if (statusTheme == null) {
+    localStorage.setItem("Theme", "light");
 }
-
-nut.onclick = h = () =>  {
-  let hienTai = body.className;
-  if (hienTai === "light") {
-    doiTheme("dark");
-  } else {
-    doiTheme("light");
-  }
-};
-
+const render = () => {
+    let getTheme = localStorage.getItem("Theme");
+    if (getTheme == "light") {
+        body.style.color = "black";
+        body.style.backgroundColor = "white";
+    }
+    else if (getTheme == "dark") {
+        body.style.color = "white";
+        body.style.backgroundColor = "black";
+    }
+}
+const changeTheme = () => {
+    let statusTheme = localStorage.getItem("Theme");
+    if (statusTheme == "light") {
+        localStorage.setItem("Theme" , "dark");
+    }
+    else if (statusTheme == "dark") {
+        localStorage.setItem("Theme", "light");
+    }
+    render();
+}
+btn.addEventListener("click", changeTheme);
+render();
